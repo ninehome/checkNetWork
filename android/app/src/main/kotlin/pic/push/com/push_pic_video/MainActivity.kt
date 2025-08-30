99b82714bb4b1aae59a2a422fe517b099d1b7551
let.s go <113565560+ninehome@users.noreply.github.com>
2025-08-30 09:57:18 +0800

    x

--- android/app/src/main/kotlin/pic/push/com/push_pic_video/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package pic.push.com.push_pic_video
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/routers/router_change_notice.dart
+++
@@ -1,7 +0,0 @@
-
-
-class RouterChange{
-  static notice() {
-
-  }
-}
